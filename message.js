20ccf5de27d217d07dbc2adc1780037671854bae
bikeshtamang1 <86940969+bikeshtamang1@users.noreply.github.com>
2024-04-23 17:40:03 +0545

    Add files via upload

---
+++ message.js
@@ -0,0 +1,7 @@
+function consoleStyler(color, background, fontSize, txt) {
+    var message = "%c" + txt;
+    var style = `color: ${color};`;
+    style += `background: ${background};`;
+    style += `font-size: ${fontSize};`;
+    console.log(message, style);
+}
